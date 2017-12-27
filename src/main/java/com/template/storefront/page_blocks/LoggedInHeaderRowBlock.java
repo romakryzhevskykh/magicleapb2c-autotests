8c025e5a245f38900418c22f5312fa8fad289dfe
mak <aleksandr.sarana@zaelab.com>
2017-12-27 15:18:30 +0200

    thread fixes + movement tests from TDSC stage to DEMO(in process)

---
+++ src/main/java/com/template/storefront/page_blocks/LoggedInHeaderRowBlock.java
@@ -0,0 +1,22 @@
+package com.sarnova.storefront.page_blocks;
+
+import com.sarnova.helpers.BasePageObject;
+import org.springframework.stereotype.Component;
+import ru.yandex.qatools.allure.annotations.Step;
+
+import static com.sarnova.storefront.page_elements.LoggedInHeaderRowBlockElements.MY_ACCOUNT_DROP_DOWM_XPATH;
+import static com.sarnova.storefront.page_elements.LoggedInHeaderRowBlockElements.SIGN_OUT_BUTTON_XPATH;
+
+@Component
+public class LoggedInHeaderRowBlock extends BasePageObject {
+
+    @Step("Check that user is logged in.")
+    public boolean isUserLoggedIn() {
+        return isDisplayed($(MY_ACCOUNT_DROP_DOWM_XPATH));
+    }
+
+    @Step("Click on sign out button.")
+    public void clickOnSignOutButton() {
+        $(SIGN_OUT_BUTTON_XPATH).click();
+    }
+}
---
+++ src/main/java/com/template/storefront/page_elements/LoggedOutHeaderRowBlockElements.java
@@ -0,0 +1,5 @@
+package com.sarnova.storefront.page_elements;
+
+public class LoggedOutHeaderRowBlockElements {
+    public static final String SIGN_IN_BUTTON_XPATH = "//a[@href][contains(., 'Sign in')]";
+}
