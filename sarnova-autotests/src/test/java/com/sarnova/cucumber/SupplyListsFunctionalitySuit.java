a22cde4c164d008ff4a38dc679a7ac4d174261ac
mak <aleksandr.sarana@zaelab.com>
2018-03-23 14:52:21 +0200

    all tests active

--- sarnova-autotests/src/test/java/com/sarnova/cucumber/SupplyListsFunctionalitySuit.java
+++ sarnova-autotests/src/test/java/com/sarnova/cucumber/SupplyListsFunctionalitySuit.java
@@ -5,9 +5,9 @@ import cucumber.api.CucumberOptions;
 
 @CucumberOptions(
         format = {"pretty"},
-        features = {//"src/test/resources/features/supply_list_features/supply_list_details_page_features/Supply_List_Details_Activation_Statuses.feature",
-//        "src/test/resources/features/supply_list_features/supply_list_details_page_features/Supply_List_Details_Favorite.feature",
-//        "src/test/resources/features/supply_list_features/supply_list_details_page_features/Supply_List_Details_Quick_Add.feature",
+        features = {"src/test/resources/features/supply_list_features/supply_list_details_page_features/Supply_List_Details_Activation_Statuses.feature",
+        "src/test/resources/features/supply_list_features/supply_list_details_page_features/Supply_List_Details_Favorite.feature",
+        "src/test/resources/features/supply_list_features/supply_list_details_page_features/Supply_List_Details_Quick_Add.feature",
                 "src/test/resources/features/supply_list_features/supply_lists_page_features/Supply_Lists_Activation_Statuses.feature",
                 "src/test/resources/features/supply_list_features/supply_lists_page_features/Supply_Lists_Favorite.feature"}
 )
