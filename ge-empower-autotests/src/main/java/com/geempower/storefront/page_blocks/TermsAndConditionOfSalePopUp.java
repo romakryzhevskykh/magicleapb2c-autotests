73a9d74e935dbabfe21dc9853a2648775027896a
romakryzhevskykh <roman.kryzhevskykh@zaelab.com>
2018-03-27 14:10:09 +0300

    fix annotations

--- ge-empower-autotests/src/main/java/com/geempower/storefront/page_blocks/TermsAndConditionOfSalePopUp.java
+++ ge-empower-autotests/src/main/java/com/geempower/storefront/page_blocks/TermsAndConditionOfSalePopUp.java
@@ -1,6 +1,8 @@
 package com.geempower.storefront.page_blocks;
 
 import com.geempower.helpers.UIComponent;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TermsAndConditionOfSalePopUp extends UIComponent {
 }
--- ge-empower-autotests/src/main/java/com/geempower/storefront/pages/OrderEntry2Page.java
+++ ge-empower-autotests/src/main/java/com/geempower/storefront/pages/OrderEntry2Page.java
@@ -1,7 +1,9 @@
 package com.geempower.storefront.pages;
 
 import com.geempower.storefront.StorefrontBasePage;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderEntry2Page extends StorefrontBasePage {
 
     private final String pageUri = "checkout";
