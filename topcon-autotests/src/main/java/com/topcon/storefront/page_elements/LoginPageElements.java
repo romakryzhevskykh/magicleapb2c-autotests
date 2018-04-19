043d36809679adc265d7bc895255c868ccef4e97
Artem Karhanov <artem.karhanov@zaelab.com>
2018-04-19 17:42:56 +0300

    Finished test for login process

--- topcon-autotests/src/main/java/com/topcon/storefront/page_elements/LoginPageElements.java
+++ topcon-autotests/src/main/java/com/topcon/storefront/page_elements/LoginPageElements.java
@@ -4,5 +4,5 @@ public class LoginPageElements {
     private static final String LOG_IN_FORM = "//form[@id='loginForm']";
     public static final String USERNAME_FIELD_XPATH = LOG_IN_FORM + "//input[@id='j_username']";
     public static final String PASSWORD_FIELD_XPATH = LOG_IN_FORM + "//input[@id='j_password']";
-    public static final String LOG_IN_BUTTON_XPATH = LOG_IN_FORM + "/button";
+    public static final String LOG_IN_BUTTON_XPATH = LOG_IN_FORM + "//button";
 }
