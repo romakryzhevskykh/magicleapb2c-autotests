b1edb9ec8b7f63f2b97ae6a36749f69dcfc4630c
romakryzhevskykh <roman.kryzhevskykh@zaelab.com>
2018-08-03 16:57:53 +0300

    locator fix

--- ge-empower-autotests/src/main/java/com/geempower/storefront/page_elements/order/OrderDetailsPageElements.java
+++ ge-empower-autotests/src/main/java/com/geempower/storefront/page_elements/order/OrderDetailsPageElements.java
@@ -1,8 +1,8 @@
 package com.geempower.storefront.page_elements.order;
 
 public class OrderDetailsPageElements {
-    public final static String GE_ORDER_NO_XPATH = ".//a[@class='text-status-grey']";
-    public final static String TOTAL_NET_PRICE_VALUE_XPATH = ".//span[@class='orderTotalPrice']";
+    public final static String GE_ORDER_NO_XPATH = "//a[@class='text-dark-grey']";
+    public final static String TOTAL_NET_PRICE_VALUE_XPATH = "//span[@class='orderTotalPrice']";
     public final static String EXPAND_ORDER_DETAILS_ICON_XPATH = "//i[@class='fa fa-angle-down table-collpase display-inline-block collapsed'][1]";
     public final static String INVOICE_DOWNLOAD_HYPERLINK_XPATH = "//div[@id='description-1-0-0']//a[@class='check-invoice-href']";
 }
