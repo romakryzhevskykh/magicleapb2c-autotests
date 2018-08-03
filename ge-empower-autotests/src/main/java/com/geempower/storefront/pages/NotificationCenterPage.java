2fe5c4331ee2c4eec93332cdba545212d58348eb
Diana <diana.sanzharevskaya@x2sy.com>
2018-08-03 17:29:35 +0300

    tests for Notification center page were added

--- ge-empower-autotests/src/main/java/com/geempower/storefront/pages/NotificationCenterPage.java
+++ ge-empower-autotests/src/main/java/com/geempower/storefront/pages/NotificationCenterPage.java
@@ -2,6 +2,7 @@ package com.geempower.storefront.pages;
 
 import com.geempower.storefront.StorefrontBasePage;
 import org.springframework.stereotype.Component;
+import ru.yandex.qatools.allure.annotations.Step;
 
 import static com.geempower.storefront.page_elements.NotificationCenterPageElements.*;
 
@@ -20,27 +21,70 @@ public class NotificationCenterPage extends StorefrontBasePage {
         return getCurrentUrl().equals(getPageUrl());
     }
 
+    @Step("Get Notification Center Page Title")
     public String getNotificationCenterPageTitle() {
         return $(NOTIFICATION_PREFERENCES_TITLE_XPATH).getText();
     }
 
+    @Step("Get Promotional Content Section Title")
     public String getPromotionalContentSectionTitle() {
         return $(PROMOTIONAL_CONTENT_TITLE_XPATH).getText();
     }
 
+    @Step("Get Informational Toggle Section Description")
     public String getInformationalToggleSectionDescription() {
         return $(INFORMATIONAL_TOGGLE_DESCRIPTION_XPATH).getText();
     }
 
+    @Step("Get Subscription Preference Description")
     public String getSubscriptionPreferenceDescription() {
         return $(SUBSCRIPTION_PREFERENCE_DESCRIPTION_XPATH).getText();
     }
 
+    @Step("Get Status Training Alerts Section Header")
     public String getStatusTrainingAlertsSectionHeader() {
         return $(STATUS_TRAINING_ALERT_HEADER_XPATH).getText();
     }
 
+    @Step("Get Notification Preferences Section Header")
     public String getNotificationPreferencesSectionHeader() {
         return $(NOTIFICATION_PREFERENCE_TITLE_XPATH).getText();
+    }
+
+    @Step("Get Lessonly Toggle Position")
+    public String getLessonlyTogglePosition() {
+        String togglePosition;
+        if ($(LESSONLY_TOGGLE_XPATH).getCssValue("background-color").equals("rgba(59, 115, 185, 1)")) {
+            togglePosition = "ON";
+        } else {
+            togglePosition = "OFF";
+        }
+        return togglePosition;
+    }
+
+    @Step("Switch The Lessonly Toggle")
+    public void switchTheLessonlyToggle() {
+        click(LESSONLY_TOGGLE_XPATH);
+    }
+
+    @Step("Save Notification Preferences")
+    public void saveNotificationPreferences() {
+        click(SAVE_NOTIFICATION_PREFERENCES_BUTTON_XPATH);
+    }
+
+    @Step("Get Status Page Toggle Position")
+    public String getStatusPageTogglePosition() {
+        String togglePosition;
+        if ($(STATUS_PAGE_TOGGLE_XPATH).getCssValue("background-color").equals("rgba(59, 115, 185, 1)")) {
+            togglePosition = "ON";
+        } else {
+            togglePosition = "OFF";
+        }
+        return togglePosition;
+    }
+
+    @Step("Switch The Status Page Toggle")
+    public void switchTheStatusPageToggle() {
+        click(STATUS_PAGE_TOGGLE_XPATH);
     }
 }
--- ge-empower-autotests/src/test/java/com/geempower/cucumber/definition_steps/NotificationCenterStepDefs.java
+++ ge-empower-autotests/src/test/java/com/geempower/cucumber/definition_steps/NotificationCenterStepDefs.java
@@ -1,7 +1,11 @@
 package com.geempower.cucumber.definition_steps;
 
+import com.geempower.helpers.ThreadVarsHashMap;
 import com.geempower.storefront.pages.NotificationCenterPage;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.testng.Assert.assertEquals;
@@ -40,5 +44,52 @@ public class NotificationCenterStepDefs extends AbstractStepDefs {
     @Then("^(.*) header of Notification Preferences section is displayed.$")
     public void notificationPreferencesSectionHeaderIsDisplayed(String sectionHeader) {
         assertTrue(notificationCenterPage.getNotificationPreferencesSectionHeader().equals(sectionHeader));
+    }
+
+    @When("^Admin switches the Lessonly toggle to another state.$")
+    public void adminSwitchesTheLessonlyToggle() {
+        String currentTogglePosition = threadVarsHashMap.getString(TestKeyword.LESSONLY_TOGGLE_POSITION);
+        notificationCenterPage.switchTheLessonlyToggle();
+        if (currentTogglePosition.equals("ON")) {
+            threadVarsHashMap.replace(TestKeyword.LESSONLY_TOGGLE_POSITION, "OFF");
+        } else if (currentTogglePosition.equals("OFF")) {
+            threadVarsHashMap.replace(TestKeyword.LESSONLY_TOGGLE_POSITION, "ON");
+        }
+    }
+
+    @And("^Current Lessonly toggle position is saved to the threadVarsHashMap.$")
+    public void currentLessonlyTogglePositionIsSavedToTheThreadVarsHashMap() {
+        threadVarsHashMap.replace(TestKeyword.LESSONLY_TOGGLE_POSITION, notificationCenterPage.getLessonlyTogglePosition());
+    }
+
+    @And("^Click on Save notification preferences button.$")
+    public void clickOnSaveNotificationPreferencesButton() {
+        notificationCenterPage.saveNotificationPreferences();
+    }
+
+    @Then("^Lessonly toggle is switched to previously chosen state.$")
+    public void lessonlyToggleIsSwitchedToPreviouslyChosenState() {
+        assertEquals(threadVarsHashMap.getString(TestKeyword.LESSONLY_TOGGLE_POSITION), notificationCenterPage.getLessonlyTogglePosition());
+    }
+
+    @And("^Current Status Page toggle position is saved to the threadVarsHashMap.$")
+    public void currentStatusPageTogglePositionIsSavedToTheThreadVarsHashMap() {
+        threadVarsHashMap.replace(TestKeyword.STATUS_PAGE_TOGGLE_POSITION, notificationCenterPage.getStatusPageTogglePosition());
+    }
+
+    @When("^Admin switches the Status Page toggle to another state.$")
+    public void adminSwitchesTheStatusPageToggleToAnotherState() {
+        String currentTogglePosition = threadVarsHashMap.getString(TestKeyword.STATUS_PAGE_TOGGLE_POSITION);
+        notificationCenterPage.switchTheStatusPageToggle();
+        if (currentTogglePosition.equals("ON")) {
+            threadVarsHashMap.replace(TestKeyword.STATUS_PAGE_TOGGLE_POSITION, "OFF");
+        } else if (currentTogglePosition.equals("OFF")) {
+            threadVarsHashMap.replace(TestKeyword.STATUS_PAGE_TOGGLE_POSITION, "ON");
+        }
+    }
+
+    @Then("^Status Page toggle is switched to previously chosen state.$")
+    public void statusPageToggleIsSwitchedToPreviouslyChosenState() {
+        assertEquals(threadVarsHashMap.getString(TestKeyword.STATUS_PAGE_TOGGLE_POSITION), notificationCenterPage.getStatusPageTogglePosition());
     }
 }
