155ff34afe96eb1ee73defa5e98aedf1401a52f9
romakryzhevskykh <roman.kryzhevskykh@zaelab.com>
2018-08-20 17:10:37 +0300

    fixes added

--- ge-empower-autotests/src/main/java/com/geempower/storefront/page_elements/ShipmentsPageElements.java
+++ ge-empower-autotests/src/main/java/com/geempower/storefront/page_elements/ShipmentsPageElements.java
@@ -1,7 +1,7 @@
 package com.geempower.storefront.page_elements;
 
 public class ShipmentsPageElements {
-    public final static String SHIPMENTS_TITLE_XPATH = ".//div[@class='col-xs-12 col-sm-2 col-md-2']";
+    public final static String SHIPMENTS_TITLE_XPATH = ".//section[@class='main-content-section shipment-dashboard-section']//h3";
     public final static String LAST_SHIPMENTS_DROPDOWN_ID = "select2-shipmentRowNumber-container";
     public final static String FILTERS_BUTTON_XPATH = ".//i[@class='fa fa-filter']";
     public final static String BELL_ICON_XPATH = ".//i[@class='fa fa-bell']";
