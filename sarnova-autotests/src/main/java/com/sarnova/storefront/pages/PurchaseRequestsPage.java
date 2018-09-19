9bf309b200743b34646123d92907fd971a62d6f7
mak <aleksandr.sarana@zaelab.com>
2018-09-19 16:22:52 +0300

    Fix master

--- sarnova-autotests/src/main/java/com/sarnova/storefront/pages/PurchaseRequestsPage.java
+++ sarnova-autotests/src/main/java/com/sarnova/storefront/pages/PurchaseRequestsPage.java
@@ -4,7 +4,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class PurchaseRequestsPage extends StorefrontBasePage {
-    private String pageUrlMethod = "my-account/orderApprovalDetails/%s";
+    private String pageUrlMethod = "my-account/approval-dashboard";
+
     @Override
     public String getPageUrl() {
         return storefrontProject.getBaseUrl() + pageUrlMethod;
