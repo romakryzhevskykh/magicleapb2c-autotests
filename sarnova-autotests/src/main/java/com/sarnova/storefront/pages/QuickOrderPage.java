03b2a47d7cdc20167c3f9ff916b76b25343f6b10
PeterWork <apscep@gmail.com>
2018-10-19 14:54:46 +0300

    Final fix commit

--- sarnova-autotests/src/main/java/com/sarnova/storefront/pages/QuickOrderPage.java
+++ sarnova-autotests/src/main/java/com/sarnova/storefront/pages/QuickOrderPage.java
@@ -137,7 +137,6 @@ public class QuickOrderPage extends StorefrontBasePage {
         waitUntilPageIsFullyLoaded();
            }
 
-
     public void clickOnPopUpResetFormButton() {
         waitUntilElementIsVisible(By.id(RESET_FORM_POP_UP_ID));
         click(RESET_FORM_BUTTON_POP_UP_XPATH);
